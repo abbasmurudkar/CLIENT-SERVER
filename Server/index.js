4d22a9c884c0cc7060cbdb5399db1a87e5693014
abbasmurudkar <abbasmurudkar18@icloud.com>
2022-03-27 18:21:46 +0530

    CONNECTION_WITH_DATABSE

--- Server/index.js
+++ Server/index.js
@@ -9,3 +9,5 @@ const app = express();
 app.use(bodyParser.json({limit: "30mb", extended: true }))  //setting the limit for body of html by encoding like images should not be greater then 30mb 
 app.use(bodyParser.urlencoded({limit : "30mb", extended: true})) //for encoding the url  
 app.use(cors()); //timestamp function that tells from where the request has send or come
+const CONNECTION_URL ="mongodb+srv://socialnetwork:<password>@cluster0.czsk3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"  // connection of the mongodb database
+const PORT = 5000;
--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,12 @@ import './App.css';
 function App() {
   return (
  <>
+ <h1>app</h1>
  </>
   );
 }
 
 export default App;
+
+// mongodb+srv://socialnetwork:<password>@cluster0.czsk3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+// mongodb+srv://socialnetwork:<password>@cluster0.czsk3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
